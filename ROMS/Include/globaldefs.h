af91d5cf41d15a25ae7ff9dce113eee8c8ef59a2
Mark Hadfield <mark.hadfield@niwa.co.nz>
2018-02-08 15:23:40 +1300

    Fixes related to ROMS-SWAN coupling.
    The INLET_TEST case in roms-test-mgh is still not working.

--- ROMS/Include/globaldefs.h
+++ ROMS/Include/globaldefs.h
@@ -567,7 +567,7 @@
 #endif
 
 #if defined CICE_COUPLING
-  define ICE_COUPLING
+# define ICE_COUPLING
 #endif
 
 #if defined REFDIF_COUPLING || defined SWAN_COUPLING || \
@@ -575,7 +575,7 @@
 # define WAV_COUPLING
 #endif
 
-#if defined ATM_COUPLING || ICE_COUPLING || WAV_COUPLING
+#if defined ATM_COUPLING || defined ICE_COUPLING || defined WAV_COUPLING
 # define MODEL_COUPLING
 #endif
 
