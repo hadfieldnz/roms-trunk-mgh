04f9a381f5b3bccd9ae7e4220e84ee73dfa005cb
arango <arango@f091316a-d328-0410-a40a-876eff57d070>
2022-01-06 21:01:01 +0000

    src:ticket:904
    git-svn-id: https://www.myroms.org/svn/src/trunk@1099 f091316a-d328-0410-a40a-876eff57d070

--- ROMS/Nonlinear/Biology/npzd_Powell.h
+++ ROMS/Nonlinear/Biology/npzd_Powell.h
@@ -2,7 +2,7 @@
 !
 !svn $Id$
 !================================================== Hernan G. Arango ===
-!  Copyright (c) 2002-2021 The ROMS/TOMS Group        Craig V. Lewis   !
+!  Copyright (c) 2002-2022 The ROMS/TOMS Group        Craig V. Lewis   !
 !    Licensed under a MIT/X style license                              !
 !    See License_ROMS.txt                                              !
 !=======================================================================
